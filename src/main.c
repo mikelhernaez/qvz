31f32647e18dffc2012e10aeec0fa580c9463f37
Greg Malysa <gmalysa@stanford.edu>
2014-09-18 15:25:28 -0700

    Improved command line options and parsing

--- src/main.c
+++ src/main.c
@@ -18,7 +18,7 @@ char *default_codebook = "codebook.txt";
 /**
  *
  */
-void encode(char *input_name, char *output_name, char *codebook_name) {
+void encode(char *input_name, char *output_name, char *codebook_name, double comp) {
 	// New variables, definitely used
 	struct distortion_t *dist = generate_distortion_matrix(41, DISTORTION_MSE);
 	struct alphabet_t *alphabet = alloc_alphabet(41);
@@ -42,7 +42,7 @@ void encode(char *input_name, char *output_name, char *codebook_name) {
 	}
     
 	training_stats = alloc_conditional_pmf_list(alphabet, training_file.columns);
-	qlist = generate_codebooks(&training_file, training_stats, dist, 0.5, &distortion);
+	qlist = generate_codebooks(&training_file, training_stats, dist, comp, &distortion);
     
 	stop_timer(&stats);
 	start_timer(&encoding);
@@ -105,48 +105,126 @@ void decode(char *input_file, char *output_file, char* codebook_file) {
  * @param name Program name string
  */
 void usage(char *name) {
-	printf("Usage: %s [mode] [codebook file] [input file] [output file]\n", name);
-	printf("Where [mode] is -c for compression and -d for decompression\n");
+	printf("Usage: %s (options) [codebook file] [input file] [output file]\n", name);
+	printf("Options are:\n");
+	printf("\t-x\t\t: Extract quality values from compressed file\n");
+	printf("\t-c\t\t: Store quality values in compressed file (default)\n");
+	printf("\t-f [ratio]\t: Compress using [ratio] bits per bit of input entropy per symbol\n");
+	printf("\t-r [rate]\t: Compress using fixed [rate] bits per symbol\n");
+	printf("\t-v\t\t: Enable verbose output\n");
+	printf("\t-s\t\t: Print summary stats\n");
+	printf("\t-h\t\t: Print this help\n");
 }
 
 /**
  *
  */
 int main(int argc, char **argv) {
-    char *input_name, *output_name, *codebook_name;
-    
-	// Check for arguments and get file names
-	if (argc != 5) {
+    char *input_name = 0;
+	char *output_name = 0;
+	char *codebook_name = 0;
+	uint32_t i;
+
+	uint8_t extract = 0;
+	uint8_t use_comp = 1;
+	uint8_t file_idx = 0;
+	uint8_t verbose = 0;
+	uint8_t stats = 0;
+	double rate = 0.5;
+
+	// No dependency, cross-platform command line parsing means no getopt
+	// So we need to settle for less than optimal flexibility (no combining short opts, maybe that will be added later)
+	i = 1;
+	while (i < argc) {
+		// Handle file names and reject any other untagged arguments
+		if (argv[i][0] != '-') {
+			switch (file_idx) {
+				case 0:
+					codebook_name = argv[i];
+					file_idx = 1;
+					break;
+				case 1:
+					input_name = argv[i];
+					file_idx = 2;
+					break;
+				case 2:
+					output_name = argv[i];
+					file_idx = 3;
+					break;
+				default:
+					printf("Garbage argument \"%s\" detected.\n", argv[i]);
+					usage(argv[0]);
+					exit(1);
+			}
+			i += 1;
+			continue;
+		}
+
+		// Flags for options
+		switch(argv[i][1]) {
+			case 'x':
+				extract = 1;
+				i += 1;
+				break;
+			case 'c':
+				extract = 0;
+				i += 1;
+				break;
+			case 'f':
+				extract = 0;
+				use_comp = 1;
+				rate = atof(argv[i+1]);
+				i += 2;
+				break;
+			case 'r':
+				extract = 0;
+				use_comp = 0;
+				rate = atof(argv[i+1]);
+				i += 2;
+				printf("--Warning-- fixed rate encoding not yet implemented, falling back to ratio");
+				break;
+			case 'v':
+				verbose = 1;
+				i += 1;
+				break;
+			case 'h':
+				usage(argv[0]);
+				exit(0);
+			case 's':
+				stats = 1;
+				i += 1;
+				break;
+			default:
+				printf("Unrecognized option -%c.\n", argv[i][1]);
+				usage(argv[0]);
+				exit(1);
+		}
+	}
+
+	if (file_idx != 3) {
+		printf("Missing required filenames.\n");
 		usage(argv[0]);
-		
-		// Attempted to run from command line but not all arguments are present
-		if (argc > 1) {
-			printf("Incorrect number of arguments supplied!\n");
-			exit(1);
+		exit(1);
+	}
+
+	if (verbose) {
+		if (extract) {
+			printf("%s will be decoded to %s.\n", input_name, output_name);
 		}
 		else {
-			printf("Using default input file: %s\nDefault output file: %s.\nDefault codebook file:%s\n", default_input, default_output, default_codebook);
-			input_name = default_input;
-			output_name = default_output;
-            codebook_name = default_codebook;
+			printf("%s will be encoded as %s.\n", input_name, output_name);
+			if (use_comp)
+				printf("Ratio mode selected, targeting %f compression ratio\n", rate);
+			else
+				printf("Fixed-rate mode selected, targeting %f bits per symbol\n", rate);
 		}
 	}
-	else {
-        codebook_name = argv[2];
-		input_name = argv[3];
-		output_name = argv[4];
+
+	if (extract) {
+		decode(input_name, output_name, codebook_name);
 	}
-    
-    if (strcmp(argv[1], "-c") == 0) {
-        encode(input_name, output_name, codebook_name);
-    }
-    else if (strcmp(argv[1], "-d") == 0) {
-        decode(input_name, output_name, codebook_name);
-    }
-    else {
-		usage(argv[0]);
-		printf("Missing required -c or -d!\n");
-		exit(1);
+	else {
+		encode(input_name, output_name, codebook_name, rate);
 	}
 
 #ifndef LINUX
