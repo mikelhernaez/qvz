58cda12239cfb3c256a3b036fb1177146924493a
Greg Malysa <gmalysa@stanford.edu>
2014-10-03 21:40:22 -0700

    Fix double-print when generating RD curves

--- generate_rd.sh
+++ generate_rd.sh
@@ -4,7 +4,7 @@ idx=0
 STATSFILE=`mktemp rd_statsXXXXXX`
 while [ $idx -lt 20 ]; do
 	comp=$(echo "$idx*0.05" | bc -l)
-	bin/qvz -c 4 -f $comp -s $1 $2 | tee -a $STATSFILE -
+	bin/qvz -c 3 -f $comp -s $1 $2 | tee -a $STATSFILE
 	idx=$((idx+1))
 done
 awk '{print $2 $4 $6}' $STATSFILE > $3
